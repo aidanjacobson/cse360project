ef1dc80e372e858bb409ba26f7dd90b9a267a3a5
natsu <natsu@Liam-SurfacePro>
2024-09-26 14:08:23 -0700

    Updated login page ui

--- demo/src/main/java/cse360project/pages/LoginPage.java
+++ demo/src/main/java/cse360project/pages/LoginPage.java
@@ -1,11 +1,9 @@
 package cse360project.pages;
 
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -13,32 +11,37 @@ import javafx.scene.text.Text;
 public class LoginPage implements Page {
     StackPane root = new StackPane();
     public LoginPage() {
-    	Text titleText = new Text("this is the login page");
-        root.getChildren().add(titleText);
-        VBox vbox = new VBox();
+        VBox vbox = new VBox(10);
+        Text titleText = new Text("this is the login page");
+        vbox.getChildren().add(titleText);
         Label label = new Label("Login here");
         vbox.getChildren().add(label);
         vbox.setAlignment(Pos.CENTER);
         final TextField username = new TextField();
         username.setPromptText("Enter Username");
-        username.setPrefColumnCount(10);
+        username.setMaxWidth(200);
         username.getText();
         vbox.getChildren().add(username);
         final TextField password = new TextField();
         password.setPromptText("Enter Password");
-        password.setPrefColumnCount(10);
+        password.setMaxWidth(200);
         password.getText();
         vbox.getChildren().add(password);
+        Button login = new Button("Login");
+        vbox.getChildren().add(login);
+        root.getChildren().add(vbox);
+        
+        VBox vbox2 = new VBox(10);
+        vbox2.setAlignment(Pos.BOTTOM_CENTER);
         final TextField invite_code = new TextField();
         invite_code.setPromptText("Enter Invite Code");
-        invite_code.setPrefColumnCount(10);
+        invite_code.setMaxWidth(200);
         invite_code.getText();
-        vbox.getChildren().add(invite_code);
-        Button login = new Button("Login");
-        Button join = new Button("Join now");
-        vbox.getChildren().add(login);
-        vbox.getChildren().add(join);
-        root.getChildren().add(vbox);
+        vbox2.getChildren().add(invite_code);
+        Button join = new Button("Join here");
+        vbox2.getChildren().add(join);
+        root.getChildren().add(vbox2);
+        
     }
 
     public void onPageOpen() {
