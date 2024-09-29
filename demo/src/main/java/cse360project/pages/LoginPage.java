78c4ec8c9bc7deec475ca8b3fb9b33850f77e9aa
natsu <natsu@Liam-SurfacePro>
2024-09-29 11:18:41 -0700

    updated login page with all code

--- demo/src/main/java/cse360project/pages/LoginPage.java
+++ demo/src/main/java/cse360project/pages/LoginPage.java
@@ -2,11 +2,19 @@ package cse360project.pages;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import cse360project.User;
+import cse360project.utils.ApplicationStateManager;
 import cse360project.utils.DatabaseHelper;
 import cse360project.utils.PageManager;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
@@ -18,9 +26,9 @@ public class LoginPage implements Page {
     public LoginPage() {
     	boolean isDatabaseEmpty = DatabaseHelper.isDatabaseEmpty();
     	if(isDatabaseEmpty) {
-//    		PageManager.registerPage("accountsetup", new AccountSetUp());
-//    		PageManager.switchToPage("accountsetup");
+    		PageManager.switchToPage("accountsetup");
     	}
+ 
         VBox vbox = new VBox(10);
         Text titleText = new Text("this is the login page");
         vbox.getChildren().add(titleText);
@@ -41,34 +49,36 @@ public class LoginPage implements Page {
         vbox.getChildren().add(login);
         root.getChildren().add(vbox);
         login.setOnAction(e -> {
-        	try {
-				PreparedStatement ps = DatabaseHelper.prepareStatement("SELECT * FROM cse360users WHERE username=?");
-			} catch (SQLException e1) {
+        try { 
+				Timestamp today = Timestamp.valueOf(LocalDateTime.now());
+        		PreparedStatement ps = DatabaseHelper.prepareStatement("SELECT * FROM cse360users WHERE username=?");
+				ps.setString(1, username.getText());
+				User user = DatabaseHelper.getOneUser(ps);
+	        	if (user != null) {
+	        		if (password.getText().equals(user.password)) {
+	        			if(user.OTP) {
+	        				if(today.before(user.OTP_expiration)) {
+	        					ApplicationStateManager.setLoggedInUser(user);
+	        		    		PageManager.switchToPage("accountsetup");
+	        				}else {
+	        			        Alert emailAlert = new Alert(AlertType.ERROR, "Your One Time Password has expired", ButtonType.OK);
+	        			        emailAlert.showAndWait();
+	        				}
+	        			}else {
+	        				ApplicationStateManager.setLoggedInUser(user);
+        		    		PageManager.switchToPage("roleselection");
+        			}
+	        		}else {
+	        			Alert emailAlert = new Alert(AlertType.ERROR, "Your Username and/or Password is invalid", ButtonType.OK);
+    			        emailAlert.showAndWait();
+	        		}
+	        	}
+        	} catch (SQLException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
         	
-        	
         });
-        /*
-         * if user exist
-         * 		if user has OTP flag
-         * 				if OTP expired
-         * 					"error expired OTP
-         * 				else 
-         * 					Login
-         * 					redirect to password rest
-         * 		else
-         * 			if !accountsetup
-         * 				login 
-         * 				redirect account set up
-         * 			else
-         * 				login
-         * 				redirect to role selection
-         * else
-         * 		clear fields
-         * 		"incorrect username or password" 			
-         */
         VBox vbox2 = new VBox(10);
         vbox2.setAlignment(Pos.BOTTOM_CENTER);
         final TextField invite_code = new TextField();
@@ -79,15 +89,24 @@ public class LoginPage implements Page {
         Button join = new Button("Join here");
         vbox2.getChildren().add(join);
         root.getChildren().add(vbox2);
-        /* join here button selected
-         * 	if invite code is valid
-         * 		login
-         * 		redirect user/pass set up screen
-         * 		send invite code
-         * 	else
-         * 		clear fields
-         * 		"Incorrect Invite Code
-         */
+        join.setOnAction(e -> {
+            try { 
+            		PreparedStatement ps2 = DatabaseHelper.prepareStatement("SELECT * FROM cse360users WHERE inviteCode=?");
+    				ps2.setString(1, invite_code.getText());
+    				User user2 = DatabaseHelper.getOneUser(ps2);
+    	        	if (user2 != null) {
+    	        		ApplicationStateManager.setLoggedInUser(user2);
+    		    		PageManager.switchToPage("userpasssetup");
+    	        	}else {
+    	        		Alert emailAlert = new Alert(AlertType.ERROR, "Your Username and/or Password is invalid", ButtonType.OK);
+    			        emailAlert.showAndWait();
+    	        	}
+            	} catch (SQLException e1) {
+    				// TODO Auto-generated catch block
+    				e1.printStackTrace();
+    			}
+            	
+            });
     }
 
     public void onPageOpen() {
