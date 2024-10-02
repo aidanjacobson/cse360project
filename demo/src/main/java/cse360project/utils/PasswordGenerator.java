1cc80a9802e88dee6a78b04fdb9767c8520dd2e5
aidanjacobson <survivalking4@gmail.com>
2024-10-02 12:20:57 -0700

    created passwordgenerator for invite codes and OTPs

--- demo/src/main/java/cse360project/utils/PasswordGenerator.java
+++ demo/src/main/java/cse360project/utils/PasswordGenerator.java
@@ -4,14 +4,24 @@ import java.util.Random;
 
 public class PasswordGenerator {
     static Random rand = new Random();
+
+    /**
+     * Generate a random password that meets all the requirements
+     * - At least 6 characters
+     * - 3 of the following: Uppercase, Lowercase, Numeric, Special
+     * @return the generated password
+     */
     public static String generate() {
         final int length = 8;
         char characters[] = new char[length];
+
+        // make sure the password contains at least one of each requirement
         characters[0] = generateLowercaseCharacter();
         characters[1] = generateUppercaseCharacter();
         characters[2] = generateNumericCharacter();
         characters[3] = generateSpecialCharacter();
 
+        // generate the remaining characters, with upper/lower twice as likely to occur as numeric/special
         for (int i = 4; i < length; i++) {
             int choice = rand.nextInt(6);
             if (choice >= 0 && choice < 2) {
@@ -25,32 +35,61 @@ public class PasswordGenerator {
             }
         }
 
+        // we have all the characters, but in a semi-predictable order. Shuffle them to make it completely random.
         shuffleCharArray(characters);
 
         return String.valueOf(characters);
     }
 
+    /**
+     * Generate a random uppercase char
+     * @return an uppercase char
+     */
     static char generateUppercaseCharacter() {
         return (char)randInclusive('A', 'Z');
     }
 
+    /**
+     * Generate a random lowercase char
+     * @return a lowercase char
+     */
     static char generateLowercaseCharacter() {
         return (char)randInclusive('a', 'z');
     }
 
+    /**
+     * Generate a random numeric char
+     * @return a numeric char
+     */
     static char generateNumericCharacter() {
         return (char)randInclusive('0', '9');
     }
 
+    /**
+     * Generate a random special character
+     * Options: ~${}%&*()
+     * @return a special char
+     */
     static char generateSpecialCharacter() {
-        String specialCharacters = "~`!@#$%^&*()_-+={}[]|\\:;\"'<>,.?/";
+        // String specialCharacters = "~`!@#$%^&*()_-+={}[]|\\:;\"'<>,.?/";
+        String specialCharacters = "!${}%&*()";
         return specialCharacters.charAt(randInclusive(0, specialCharacters.length()-1));
     }
 
+    /**
+     * Generate a random int between a and b, inclusive.
+     * @param a The lower limit
+     * @param b The upper limit
+     * @return the random int
+     */
     static int randInclusive(int a,  int b) {
         return rand.nextInt(b - a + 1) + a;
     }
 
+    /**
+     * Given a char array, shuffle it in-place to a random order.
+     * @param array the array to shuffle.
+     */
     static void shuffleCharArray(char[] array) {
         for (int i = 0; i < array.length; i++) {
             int randomIndexToSwap = rand.nextInt(array.length);
@@ -58,12 +97,5 @@ public class PasswordGenerator {
             array[randomIndexToSwap] = array[i];
             array[i] = temp;
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(generate());
-        System.out.println(generate());
-        System.out.println(generate());
-        System.out.println(generate());
     }
 }
