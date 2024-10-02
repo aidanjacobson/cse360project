91a29dcf311e29a2987432cb047d52c8881c48a0
aidanjacobson <survivalking4@gmail.com>
2024-10-02 12:20:39 -0700

    updated testroleselection

--- demo/src/main/java/test/TestRoleSelection.java
+++ demo/src/main/java/test/TestRoleSelection.java
@@ -16,4 +16,17 @@ public class TestRoleSelection {
         ApplicationStateManager.setLoggedInUser(user);
         PageManager.switchToPage("roleselection");
     }
+
+    public static void testFromStart() {
+        DatabaseHelper.setDatabasePath("~/testdb");
+        DatabaseHelper.connectToDatabase();
+        DatabaseHelper.deleteAllUsers();
+
+        User user = new User(-1, "admin", "password", "admin@cse360.com", null, true, false, null, "a_first", "a_mid", "a_last", "a_pref", true, true, true);
+        User student = new User(-1, "student", "password", "student@cse360.com", null, true, false, null, "s_first", null, "s_last", "s_pref", false, false, true);
+        User instructor = new User(-1, "instructor", "password", "instructor@cse360.com", null, true, false, null, "i_first", "i_mid", "i_last", null, false, true, false);
+        DatabaseHelper.addUser(user);
+        DatabaseHelper.addUser(student);
+        DatabaseHelper.addUser(instructor);
+    }
 }
