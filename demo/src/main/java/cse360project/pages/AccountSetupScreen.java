497e036ecbddc5522acfc322742a13c15fc7a832
aidanjacobson <survivalking4@gmail.com>
2024-10-03 14:12:24 -0700

    import validationhelper

--- demo/src/main/java/cse360project/pages/AccountSetupScreen.java
+++ demo/src/main/java/cse360project/pages/AccountSetupScreen.java
@@ -7,6 +7,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import cse360project.utils.ValidationHelper;
+
 
 public class AccountSetupScreen implements Page {
     StackPane root = new StackPane();
