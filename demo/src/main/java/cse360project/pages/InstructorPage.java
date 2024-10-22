ea76cc3327c10d6704bae997bfca8e08912e5139
aidanjacobson <survivalking4@gmail.com>
2024-10-21 19:05:30 -0700

    removed unused import

--- demo/src/main/java/cse360project/pages/InstructorPage.java
+++ demo/src/main/java/cse360project/pages/InstructorPage.java
@@ -9,7 +9,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
