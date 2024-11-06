9d184bf9914ea8d6c3a561796143afa775fd13e2
imtop <imtop@10.144.28.32>
2024-11-06 15:11:42 -0700

    EncryptionUtils

---
+++ demo/src/main/java/cse360project/utils/EncryptionUtils.java
@@ -0,0 +1,47 @@
+package cse360project.utils;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class EncryptionUtils {
+    private static final String ALGORITHM = "AES";
+    private static final String TRANSFORMATION = "AES";
+    private static final SecretKey SECRET_KEY = generateKey();
+
+ // Generate a 128-bit key
+    private static SecretKey generateKey() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
+            keyGen.init(128);
+            return keyGen.generateKey();
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating encryption key", e);
+        }
+    }
+
+    public static String encryptString(String input) {
+        try {
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(Cipher.ENCRYPT_MODE, SECRET_KEY);
+            byte[] encryptedBytes = cipher.doFinal(input.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error encrypting string", e);
+        }
+    }
+
+    public static String decryptString(String input) {
+        try {
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(Cipher.DECRYPT_MODE, SECRET_KEY);
+            byte[] decodedBytes = Base64.getDecoder().decode(input);
+            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error decrypting string", e);
+        }
+    }
+}
