f92b44307a53c1dcb17a4ab31651f3603651b379
aidanjacobson <survivalking4@gmail.com>
2024-11-07 15:12:51 -0700

    fix encryption to use only one key

--- demo/src/main/java/cse360project/utils/EncryptionUtils.java
+++ demo/src/main/java/cse360project/utils/EncryptionUtils.java
@@ -13,13 +13,11 @@ public class EncryptionUtils {
 
  // Generate a 128-bit key
     private static SecretKey generateKey() {
-        try {
-            KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
-            keyGen.init(128);
-            return keyGen.generateKey();
-        } catch (Exception e) {
-            throw new RuntimeException("Error generating encryption key", e);
-        }
+        byte[] keyBytes = new byte[] {
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
+        return new SecretKeySpec(keyBytes, ALGORITHM);
     }
 
     
