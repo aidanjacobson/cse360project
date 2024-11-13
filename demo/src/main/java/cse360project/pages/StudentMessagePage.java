74e55dd27639cb377aeb60a9701c4acdc22961f6
natsu <natsu@Liam-SurfacePro>
2024-11-13 15:08:38 -0700

    test push

--- demo/src/main/java/cse360project/pages/StudentMessagePage.java
+++ demo/src/main/java/cse360project/pages/StudentMessagePage.java
@@ -1,43 +1,92 @@
 package cse360project.pages;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
+import javafx.scene.control.Label;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import cse360project.Message;
+import cse360project.User;
+import cse360project.utils.ApplicationStateManager;
+import cse360project.utils.DatabaseHelper;
+import cse360project.utils.MessageType;
+import cse360project.utils.MessengerUtils;
 import cse360project.utils.PageManager;
+import cse360project.utils.Role;
 
 public class StudentMessagePage implements Page {
-	StackPane root = new StackPane();
+	BorderPane root = new BorderPane();
 	
-	public StudentMessagePage() {
+	public StudentMessagePage() throws SQLException {
 		VBox mainLayout = new VBox(10);
         mainLayout.setAlignment(Pos.TOP_LEFT);
         VBox messageContent = new VBox();
         ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setFitToWidth(true);
-        messageContent.setSpacing(10);
-        scrollPane.setContent(messageContent);
-//        ArrayList<Message> messages = getAllMessagesInUserThread(ApplicationStateManager.getLoggedInUser());
+        DatabaseHelper.deleteAllMessages();
+        DatabaseHelper.deleteAllUsers();
+        DatabaseHelper.createMessagesTable();
+        DatabaseHelper.createTables();
+        User sender = new User(-1, "senderUser", "password".toCharArray(), "sender@example.com", null, true, false, null, "John", null, "Doe", "Johnny", false, false, true, new ArrayList<>());
+        User threadUser = new User(-1, "threadUser", "password".toCharArray(), "thread@example.com", null, true, false, null, "Jane", null, "Doe", "Janie", false, false, true, new ArrayList<>());
+        DatabaseHelper.addUser(sender);
+        DatabaseHelper.addUser(threadUser);
+        Message messag = new Message(MessageType.SPECIFIC, "Test message content", sender, Role.STUDENT, threadUser, new Timestamp(System.currentTimeMillis()));
+        Message messag2 = new Message(MessageType.SPECIFIC, "Test message content", threadUser, Role.INSTRUCTOR, sender, new Timestamp(System.currentTimeMillis()));
+        DatabaseHelper.addMessage(messag);
+        DatabaseHelper.addMessage(messag2);
+        ApplicationStateManager.setLoggedInUser(sender);
+        ArrayList<Message> messages = MessengerUtils.getAllMessagesInUserThread(ApplicationStateManager.getLoggedInUser());
+        for (int i = 0; i < messages.size(); i++) {
+        	VBox message = new VBox();
+        	Label text = new Label(messages.get(i).getMessageContent());
+        	Label Sender = new Label(messages.get(i).getSender().getUserName() + ":");
+        	message.getChildren().add(Sender);
+        	message.getChildren().add(text);
+        	if (messages.get(i).getSender() == ApplicationStateManager.getLoggedInUser()) {
+        		
+        		message.setAlignment(Pos.BOTTOM_CENTER);
+        	}
+        	else {
+        		message.setAlignment(Pos.BOTTOM_RIGHT);
+        	}
+        	messageContent.getChildren().add(message);
+        }
 		 Button backButton = new Button("Back");
+		 mainLayout.setMaxWidth(10);
 	        backButton.setOnAction(e -> {
 	            PageManager.switchToPage("student"); // Switch to login page
 	        });
+	        // add new message button open to page thing a magig
+	        messageContent.setAlignment(Pos.CENTER);
+	        mainLayout.getChildren().add(backButton);
+	        scrollPane.setContent(messageContent);
+	        scrollPane.setMaxWidth(700);
+	        scrollPane.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
 	        
-	        
-	        mainLayout.getChildren().addAll(backButton);
 	        root.getChildren().add(mainLayout);
+	        BorderPane.setMargin(scrollPane, new Insets(100, 100, 100, 100));
+	        root.setCenter(scrollPane);
 	}
 	
 	
-	public StackPane getRoot() {
+	public BorderPane getRoot() {
         return root;
     }
 
 
 	@Override
 	public void onPageOpen() {
-
+		// TODO Auto-generated method stub
+		
 	}
 }
+
+
